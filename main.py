2316d81d05d2f03cd7a90f9e51d09572e2e318bd
dush1729 <dush1729@gmail.com>
2019-03-29 00:32:32 +0530

    Get unsolved problems list

--- main.py
+++ main.py
@@ -2,23 +2,51 @@ import sys
 import json
 import urllib.request
 
+API = "https://codeforces.com/api/"
+
 def checkResponse(status):
-	if(status == "OK"):
-		print("Successfully connected. Fetching data..")
-	else:
+	if(status != "OK"):
 		print("Something went wrong. Please try again after some time!")
 		sys.exit(0)
 
 def getContestList():
-	url = 'https://codeforces.com/api/user.rating?handle=' + handle
+	url = API + "user.rating?handle=" + handle
 	data = json.load(urllib.request.urlopen(url))
 	checkResponse(data["status"])
-	return [contest["contestId"] for contest in data["result"]]
+	return [str(contest["contestId"]) for contest in data["result"]]
 
-#Execution starts from here
+def getUnsolvedProblems(contests):
+	parsedContest = 0
+	for contestId in contests:
+		# Get index of all problems in given contest like A, B, C1, C2 etc
+		url = API + "contest.standings?contestId=" + contestId + "&handles=" + handle + "&showUnofficial=true"
+		data = json.load(urllib.request.urlopen(url))
+		checkResponse(data["status"])
+		problemList = []
+		for problem in data["result"]["problems"]:
+			problemList.append(problem["index"])
+
+		# Get solved problems in current contest
+		url = API + "contest.status?contestId=" + contestId + "&handle=" + handle + "&showUnofficial=true"
+		data = json.load(urllib.request.urlopen(url))
+		checkResponse(data["status"])
+		solved = set()
+		for submission in data["result"]:
+			if(submission["verdict"] == "OK"):
+				solved.add(submission["problem"]["index"])
+
+		# Delete solved problems
+		for problem in solved:
+			problemList.remove(problem)
+
+		parsedContest += 1
+		print("Parsed " + str(parsedContest) + " of " + str(len(contests)) + " contests.")
+
+# Execution starts from here
 if(len(sys.argv) < 2):
 	print("Please enter your codeforces handle as first argument.")
 	sys.exit(0)
 
 handle = sys.argv[1]
 contests = getContestList()
+getUnsolvedProblems(contests)
